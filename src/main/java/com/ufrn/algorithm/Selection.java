c982c574c39b6c72e03817a14b9af18d92542361
João <joao.mello.100@ufrn.edu.br>
2025-12-08 17:50:51 -0300

    feat: Seleção por Torneio e Renovação por Elitismo

--- src/main/java/com/ufrn/algorithm/Selection.java
+++ src/main/java/com/ufrn/algorithm/Selection.java
@@ -2,9 +2,39 @@ package com.ufrn.algorithm;
 
 import com.ufrn.miscs.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Selection {
+    static public List<Pair<List<Integer>, Double>> tournamentSelection(List<Pair<List<Integer>, Double>> population, int numParents, int k) {
+        Random random = new Random();
+        List<Pair<List<Integer>, Double>> parents = new ArrayList<>();
+
+        // Loop para selecionar a quantidade desejada de pais
+        for (int i = 0; i < numParents; i++) {
+            // Inicia o torneio com aleatório
+            int randomIndex = random.nextInt(population.size());
+            Pair<List<Integer>, Double> winner = population.get(randomIndex);
+
+            // Realiza as batalhas
+            for (int j = 0; j < k - 1; j++) {
+                int challengerIndex = random.nextInt(population.size());
+                Pair<List<Integer>, Double> challenger = population.get(challengerIndex);
+
+                // Menor custo vence
+                if (challenger.value() < winner.value()) {
+                    winner = challenger;
+                }
+            }
+
+            // Adiciona vencedor à lista de pais
+            parents.add(winner);
+        }
+
+        return parents;
+    }
+
     /**
     * @param population: List of pairs of routes, and their costs sorted by them
       @param percent: Percent of the population that will become parents

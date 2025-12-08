d0cedd0b0f06b3b86f22edfb5296040d650729b9
Davi Duarte <93225780+davidonigit@users.noreply.github.com>
2025-12-08 12:59:45 -0300

    feat: rodando os problemas para IMP

--- src/main/java/com/ufrn/IMPApp.java
+++ src/main/java/com/ufrn/IMPApp.java
@@ -11,8 +11,35 @@ import java.util.List;
 public class IMPApp {
     public static void main( String[] args ) {
         try {
+            String planilha = "/planilha.xlsx";
+            String planilhaProblema9e10 = "/planilhaProblema9e10.xlsx";
+
+            rodarProblema(planilha, 48, 0);
+            rodarProblema(planilha, 48, 1);
+
+            rodarProblema(planilha, 36, 0);
+            rodarProblema(planilha, 36, 1);
+
+            rodarProblema(planilha, 24, 0);
+            rodarProblema(planilha, 24, 1);
+
+            rodarProblema(planilha, 12, 0);
+            rodarProblema(planilha,12, 1);
+
+            rodarProblema(planilhaProblema9e10, 7, 0);
+            rodarProblema(planilhaProblema9e10, 7, 1);
+
+            rodarProblema(planilha, 6, 0);
+            rodarProblema(planilha, 6, 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void rodarProblema(String planilha, int numVertice, int aba){
+        try {
             LeitorExcel leitor = new LeitorExcel();
-            Grafo grafo = leitor.popularGrafo("/planilha.xlsx", 48, 0);
+            Grafo grafo = leitor.popularGrafo("/planilha.xlsx", numVertice, aba);
             System.out.println("Grafo carregado com " + grafo.getNumVertices() + " vértices.");
 
             System.out.println("\n--- 1. Insercao Mais Próxima ---");
@@ -21,7 +48,11 @@ public class IMPApp {
 
             double distanciaInicial = Miscs.calcularCustoRota(grafo, rotaInicial);
             System.out.println("Rota inicial: " + rotaInicial);
-            System.out.printf("Distância inicial: %.2f km\n", distanciaInicial);
+            if (aba == 0)
+                System.out.printf("Distância final: %.2f km\n", distanciaInicial);
+            else{
+                System.out.printf("Tempo final: %.2f min\n", distanciaInicial);
+            }
 
             BuscaLocal bl = new BuscaLocal();
             System.out.println("\n--- 2. Busca Local ---");
@@ -29,8 +60,14 @@ public class IMPApp {
             double distanciaFinal = Miscs.calcularCustoRota(grafo, rotaOtimizada);
 
             System.out.println("Rota otimizada: " + rotaOtimizada);
-            System.out.printf("Distância final: %.2f km\n", distanciaFinal);
+            
+            if (aba == 0)
+                System.out.printf("Distância final: %.2f km\n", distanciaFinal);
+            else{
+                System.out.printf("Tempo final: %.2f min\n", distanciaFinal);
+            }
             System.out.printf("Melhoria: %.2f%%\n", ((distanciaInicial - distanciaFinal) / distanciaInicial) * 100);
+            System.out.println("---------------------------------------");
         } catch (Exception e) {
             e.printStackTrace();
         }

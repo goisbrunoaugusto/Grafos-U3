590369ba9bfc45aab971b3bf4ec7d9387d6fe8fd
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2025-12-08 20:43:38 -0300

    feature: onePoint function at Crossover class

--- src/main/java/com/ufrn/algorithm/Crossover.java
+++ src/main/java/com/ufrn/algorithm/Crossover.java
@@ -3,23 +3,32 @@ package com.ufrn.algorithm;
 import com.ufrn.miscs.Miscs;
 import com.ufrn.miscs.Pair;
 import com.ufrn.model.Grafo;
+import org.apache.poi.poifs.property.Parent;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Crossover {
-    static public List<Pair<List<Integer>, Double>> onePoint(Grafo grafo, List<Pair<List<Integer>, Double>> parents){
+    static public List<Pair<List<Integer>, Double>> onePoint(Grafo grafo, List<Pair<List<Integer>, Double>> parentList){
         Random random = new Random();
-        int point = random.nextInt(0, parents.getFirst().key().size());
+        int point = random.nextInt(0, parentList.getFirst().key().size());
 
         List<Pair<List<Integer>, Double>> offspringList = new ArrayList<>();
-        for (int i = 0; i < parents.size(); i = i + 2) {
-            List<Integer> parentA = parents.get(i).key();
-            List<Integer> parentB = parents.get(i+1).key();
-            List<Integer> parentAChromosomes = new ArrayList<>(parentA.subList(0,point-1));
-            List<Integer> parentBChromosomes = new ArrayList<>(parentB.subList(point,parents.get(i+1).key().size()-1));
+        for (int i = 0; i < parentList.size(); i = i + 2) {
+            List<Integer> parentA = parentList.get(i).key();
+            List<Integer> parentB = parentList.get(i+1).key();
+            List<Integer> parentAChromosomes = new ArrayList<>(parentA.subList(0,point));
+            List<Integer> parentBChromosomes = new ArrayList<>(parentB.subList(point,parentList.get(i+1).key().size()));
             List<Integer> offspringChromosomes = new ArrayList<>(parentAChromosomes);
+
+            Queue<Integer> notOnListQueue = new LinkedList<>();
+            for (int j = 0; j < parentA.size(); j++) {
+                if(!offspringChromosomes.contains(j)) notOnListQueue.add(j);
+            }
+
+            for (int j = 0; j < parentBChromosomes.size(); j++) {
+                if(parentAChromosomes.contains(parentBChromosomes.get(j))) parentBChromosomes.set(j, notOnListQueue.remove());
+            }
+
             offspringChromosomes.addAll(parentBChromosomes);
 
             Pair<List<Integer>, Double> offspring = new Pair<>(offspringChromosomes, Miscs.calcularCustoRota(grafo, offspringChromosomes));

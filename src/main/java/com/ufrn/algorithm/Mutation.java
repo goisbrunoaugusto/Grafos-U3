ec2bfc31646debeb00cf04f1fbb26934b0ca6a5e
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2025-12-08 15:38:22 -0300

    fix: Updated route cost after mutation

--- src/main/java/com/ufrn/algorithm/Mutation.java
+++ src/main/java/com/ufrn/algorithm/Mutation.java
@@ -1,5 +1,6 @@
 package com.ufrn.algorithm;
 
+import com.ufrn.miscs.Miscs;
 import com.ufrn.miscs.Pair;
 import com.ufrn.model.Grafo;
 
@@ -7,14 +8,17 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Mutation {
-    static public List<Pair<List<Integer>, Integer>> mutate(Grafo grafo, List<Pair<List<Integer>, Integer>> originalOffspringList, double percent){
+    static public List<Pair<List<Integer>, Double>> mutate(Grafo grafo, List<Pair<List<Integer>, Double>> originalOffspringList, double percent){
         Random random = new Random();
         int mutatedOffspringSize = (int) Math.floor(originalOffspringList.size()*percent);
         Set<Integer> randomRouteIndexes = random.ints(0, originalOffspringList.size()).distinct().limit(mutatedOffspringSize).boxed().collect(Collectors.toSet());
 
-        List<Pair<List<Integer>, Integer>> mutatedOffspringList = new ArrayList<>(originalOffspringList);
+        List<Pair<List<Integer>, Double>> mutatedOffspringList = new ArrayList<>(originalOffspringList);
         for(int routeIndex : randomRouteIndexes){
             Collections.swap(mutatedOffspringList.get(routeIndex).key(), random.nextInt(0, originalOffspringList.getFirst().key().size()), random.nextInt(0, originalOffspringList.getFirst().key().size()));
+            Pair<List<Integer>, Double> mutatedOffspring = new Pair<>(mutatedOffspringList.get(routeIndex).key(), Miscs.calcularCustoRota(grafo, mutatedOffspringList.get(routeIndex).key()));
+
+            mutatedOffspringList.set(routeIndex, mutatedOffspring);
         }
 
         return mutatedOffspringList;

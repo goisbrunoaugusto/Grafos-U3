a72f507975e2592e651334588196c2b6c477bed9
Davi Duarte <93225780+davidonigit@users.noreply.github.com>
2025-12-10 18:44:48 -0300

    feat: Selection Elitismo híbrida

--- src/main/java/com/ufrn/algorithm/Selection.java
+++ src/main/java/com/ufrn/algorithm/Selection.java
@@ -2,6 +2,9 @@ package com.ufrn.algorithm;
 
 import com.ufrn.miscs.Pair;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Selection {
@@ -11,14 +14,34 @@ public class Selection {
       @return Retorna uma lista com as melhores rotas escolhidas para serem pais.
     * */
     static public List<Pair<List<Integer>, Double>> elitismSelection(List<Pair<List<Integer>, Double>> population, double percent){
-        // Determina a quantidade de rotas pais
-        int parentSize = (int) Math.floor(population.size()*percent);
 
-        // Caso a quantidade seja ímpar, adiciona um pai a mais
-        if (parentSize % 2 != 0) parentSize++;
+      // Limita a porcentagem máxima a 50%
+      if (percent > .5) percent = .5;
 
-        if(parentSize <= 0) return null;
+      // Determina a quantidade de rotas pais
+      int parentSize = (int) Math.floor(population.size()*percent);
 
-        return population.subList(0, parentSize);
+      // Caso a quantidade seja ímpar, remove um pai
+      if (parentSize % 2 != 0) parentSize--;
+
+      if(parentSize <= 0) return null;
+
+      // População elite
+      List<Pair<List<Integer>, Double>> elite = population.subList(0, parentSize);
+
+      // População restante (quantidade igual a da elite)
+      List<Pair<List<Integer>, Double>> restPopulation = population.subList(parentSize, elite.size()*2);
+
+      // Embaralha a população restante para seleção aleatória
+      Collections.shuffle(restPopulation);
+
+      // Pares de pais (Elite + Aleatório)
+      List<Pair<List<Integer>, Double>> parentList = new ArrayList<>();
+      for (int i = 0; i < parentSize; i++) {
+        parentList.add(elite.get(i));
+        parentList.add(restPopulation.get(i));
+      }
+
+      return parentList;
     }
 }
